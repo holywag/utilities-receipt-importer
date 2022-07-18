1be59cf7c573162b37d1f5b55f5313334a8f0bc0
Maksym Lopatin <holywag@gmail.com>
2022-07-18 15:30:39 +0300

    Check if receipts/links already exist

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 
-from datetime import datetime
 from selenium import webdriver
 from monobank import Monobank
 from check_gov_ua import CheckGovUa
@@ -15,65 +14,62 @@ parser.add_argument('credentials_file_path', help='file containing google oath c
 parser.add_argument('spreadsheet_id', help='spreadsheet where shared links are to be added')
 args = parser.parse_args()
 
-PAYEE_TO_SERVICE_NAME = {
-    'Електроенергія': 'electric',
-    'Газ': 'gas',
-    'Газ (доставлення)': 'gas_delivery',
-    'Холодна вода': 'water',
-    'Холодна вода (водовідведення)': 'water_delivery'
-}
-
-print('Requesting statements info from monobank')
-
-monobank = Monobank(args.monobank_token)
-account_id = monobank.request_account_id(args.iban)
-statements = monobank.request_statements_for_last_n_days(account_id, 30)
-
-utility_statements = []
-
-print("Starting Safari webdriver")
-with webdriver.Safari() as driver:
-    check_gov_ua = CheckGovUa(driver)
-    for stmt in filter(lambda s: s['description'] in PAYEE_TO_SERVICE_NAME, statements):
-        print(f'Getting download link for statement {stmt["receiptId"]}')
-        recaptcha_token = check_gov_ua.get_recaptcha_token()
-        receipt_url = check_gov_ua.request_download_link('monobank', stmt['receiptId'], recaptcha_token)
-        stmt['receiptUrl'] = receipt_url
-        utility_statements.append(stmt)
-
-creds = oauth.GoogleOAuth(args.credentials_file_path).authenticate(
+google_creds = oauth.GoogleOAuth(args.credentials_file_path).authenticate(
     oauth.GoogleOAuthScopes.DRIVE_FILE, oauth.GoogleOAuthScopes.SHEETS)
 
-drive = gdrive.GoogleDriveApi(creds)
-sheets = gsheets.GoogleSheetsApi(creds)
+drive = gdrive.GoogleDriveApi(google_creds)
+sheets = gsheets.GoogleSheetsApi(google_creds)
+monobank = Monobank(args.monobank_token)
+check_gov_ua = None
 
-for stmt in utility_statements:
-    service_name = PAYEE_TO_SERVICE_NAME[stmt['description']]
-    time = datetime.fromtimestamp(int(stmt['time']))
-    receipt_file_name = f'{time.year}_{time.month-1:02d}_{service_name}.pdf'
+print('Requesting statements from monobank')
 
-    utils.download_file(stmt["receiptUrl"], receipt_file_name)
+account_id = monobank.request_account_id(args.iban)
+statements = monobank.request_statements_for_last_n_days(account_id, 20)
 
-    # todo: check if such file already exists
-    print(f'Uploading {receipt_file_name} to Google Drive')
-    file_id = drive.upload_file_to_directory(receipt_file_name, 'application/pdf', args.google_drive_directory_id)
+print('Synchronizing with Google Drive')
 
-    print(f'Removing temporary file {receipt_file_name}')
-    os.remove(receipt_file_name)
+for stmt in filter(utils.is_utility_statement, statements):
+    service_name = utils.get_service_name(stmt)
+    receipt_file_name = utils.make_receipt_file_name(service_name, stmt)
+    google_drive_id = drive.find_first_file(
+        receipt_file_name, parent_directory_id=args.google_drive_directory_id, mime_type='application/pdf')
 
-    print(f'Enabling link sharing for {receipt_file_name}')
-    drive.enable_link_sharing(file_id, "reader", "anyone")
+    if google_drive_id is None:
+        print(f'Receipt for {service_name} was not found in Google Drive, trying to upload it')
+        print(f'Getting download link for statement {stmt["receiptId"]}')
+        if check_gov_ua is None:
+            print("Starting Safari webdriver")
+            check_gov_ua = CheckGovUa(webdriver.Safari())
+        recaptcha_token = check_gov_ua.get_recaptcha_token()
+        receipt_url = check_gov_ua.request_download_link('monobank', stmt['receiptId'], recaptcha_token)
+        
+        print(f'Downloading from {receipt_url}')
+        utils.download_file(receipt_url, receipt_file_name)
 
-    shared_link = f'https://drive.google.com/file/d/{file_id}/view?usp=sharing'
+        print(f'Uploading {receipt_file_name} to Google Drive')
+        google_drive_id = drive.upload_file_to_directory(
+            receipt_file_name, 'application/pdf', args.google_drive_directory_id)
 
-    print(f'Adding {shared_link} to Google Sheets')
+        print(f'Removing temporary file {receipt_file_name}')
+        os.remove(receipt_file_name)
+
+    permissions = drive.get_file_permissions(google_drive_id)
+    if permissions is None or not utils.contains_permission(permissions, 'anyone', 'reader'):
+        print(f'Enabling link sharing for {receipt_file_name}')
+        drive.enable_link_sharing(google_drive_id, 'reader', 'anyone')
+
+    shared_link = f'https://drive.google.com/file/d/{google_drive_id}/view?usp=sharing'
+
+    print(f'Verifying that the link to {receipt_file_name} is added to Google Sheets')
     range_values = sheets.get_range(args.spreadsheet_id, f'{service_name}!G:G', gsheets.ValueRenderOption.FORMULA)
     hyperlink_formula = f'=HYPERLINK("{shared_link}", {-stmt["amount"]/100})'
-    values = sheets.update_range(
-        args.spreadsheet_id,
-        f'{service_name}!G{len(range_values)}',
-        [[hyperlink_formula]],
-        gsheets.ValueInputOption.USER_ENTERED,
-        True,
-        gsheets.ValueRenderOption.UNFORMATTED_VALUE)
-    print(f'Link to {service_name} receipt for {values[0][0]} UAH has been added to the spreadsheet')
+    if hyperlink_formula != range_values[-1][0]:
+        values = sheets.update_range(
+            args.spreadsheet_id,
+            f'{service_name}!G{len(range_values)}',
+            [[hyperlink_formula]],
+            gsheets.ValueInputOption.USER_ENTERED,
+            True,
+            gsheets.ValueRenderOption.UNFORMATTED_VALUE)
+        print(f'Link to {service_name} receipt for {values[0][0]} UAH has been added to the spreadsheet')
--- utils.py
+++ utils.py
@@ -1,8 +1,59 @@
+from datetime import datetime
 import requests
+
+UTILITY_SERVICES = {
+    'Електроенергія': {
+        'service_name': 'electric',
+        'mcc': 4900,
+    },
+    'Газ': {
+        'service_name': 'gas',
+        'mcc': 4900,
+    },
+    'Газ (доставлення)': {
+        'service_name': 'gas_delivery',
+        'mcc': 4900,
+    },
+    'Холодна вода': {
+        'service_name': 'water',
+        'mcc': 4900,
+    },
+    'Холодна вода (водовідведення)': {
+        'service_name': 'water_delivery',
+        'mcc': 4900,
+    },
+    'I\u043d\u0448\u0435': {
+        'service_name': 'water_service_fee',
+        'mcc': 4900,
+    },
+    '536354****1179': {
+        'service_name': 'rent',
+        'mcc': 4829,
+    }
+}
+
+def contains_permission(permissions, type, role): 
+    for p in permissions:
+        if p['type'] == type and p['role'] == role:
+            return True
+    return False
+
+def get_service_name(stmt):
+    svc = UTILITY_SERVICES.get(stmt['description'])
+    return svc['service_name'] if svc and stmt['mcc'] == svc['mcc'] else None
+
+def is_utility_statement(stmt):
+    return get_service_name(stmt) is not None
+
+def make_receipt_file_name(service_name, stmt):
+    time = datetime.fromtimestamp(int(stmt['time']))
+    if service_name == 'rent':
+        return f'rent_from_{time.year}_{time.month:02d}_18_to_{time.year}_{time.month+1:02d}_17.pdf'
+    else:
+        return f'{time.year}_{time.month-1:02d}_{service_name}.pdf'
 
 def download_file(url, output_file_path):
     for i in range(5):
-        print(f'Downloading {output_file_path} from {url}')
         response = requests.get(url)
         if response.status_code == 200:
             with open(output_file_path, 'wb') as file:
